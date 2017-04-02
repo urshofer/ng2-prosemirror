06405f1230f7ef3b0ff54a51c80515ff57513d8a
urshofer <u.hofer@tr51.org>
2017-04-02 09:49:42 +0200

    added lib
    since it should work from github directly

---
+++ lib/prosemirror.component.d.ts
@@ -0,0 +1,42 @@
+import { EventEmitter, OnChanges, SimpleChange } from '@angular/core';
+/**
+ * Prosemirror component
+ * Usage :
+ * <prosemirror [(ngModel)]="data" [config]="{...}"></prosemirror>
+ */
+export declare class ProsemirrorComponent implements OnChanges {
+    data: any;
+    config: any;
+    dataChange: EventEmitter<number>;
+    change: EventEmitter<{}>;
+    focus: EventEmitter<{}>;
+    blur: EventEmitter<{}>;
+    host: any;
+    instance: any;
+    props: any;
+    storeTimeout: any;
+    ngOnChanges(changes: {
+        [propKey: string]: SimpleChange;
+    }): void;
+    /**
+     * Constructor
+     */
+    constructor();
+    /**
+     * On component destroy
+     */
+    ngOnDestroy(): void;
+    /**
+     * On component view init
+     */
+    ngAfterViewInit(): void;
+    setContent(val: any): void;
+    /**
+     * Content to Markdown Serializer
+     */
+    getContent: (tr: any) => void;
+    /**
+     * Initialize prosemirror
+     */
+    prosemirrorInit(config: any): void;
+}
